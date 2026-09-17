dfb1797453a709ac1cafffa08233cb4b9b9b11b2
Karla-rmz <karamirez0823@gmail.com>
2026-09-17 12:50:16 -0600

    Manejo de mensajes y archivo README agregado

--- busqueda.sh
+++ busqueda.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Validación: debe recibir exactamente una carpeta
+# Validación
 if [ $# -eq 0 ]; then
     echo "Uso: $0 <carpeta>"
     exit 1
@@ -11,8 +11,11 @@ if [ ! -d "$1" ]; then
     exit 1
 fi
 
+#Busqueda
+
 resultado=$(grep -rl "home" "$1")
 
+#Mensajes
 if [ -z "$resultado" ]; then
     echo "No se encontró la palabra 'home' en ningún archivo de $1"
 else
